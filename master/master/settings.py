3ed7806b43425bff93ce1f9c6e3fb43d5450b40e
solon <milimi974@gmail.com>
2018-03-20 23:05:21 +0100

    Ajout du template du cahier des charges

--- master/master/settings.py
+++ master/master/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'master.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -126,4 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 INTERNAL_IPS = ['127.0.0.1']
--- master/master/views.py
+++ master/master/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 #Homepage view
 def home(request):
 
-    return render(request, 'home.html')
+    return render(request, 'pages/index.html')
