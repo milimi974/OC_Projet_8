a188ed52ff4057a32ba8f3590bf8ef58ee1d196b
solon <milimi974@gmail.com>
2018-04-09 16:30:54 +0200

    debug heroku

--- master/settings/__init__.py
+++ master/settings/__init__.py
@@ -137,7 +137,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, '/static/'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 
@@ -150,7 +150,7 @@ if os.environ.get('ENV') == 'PRODUCTION':
     PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static_cdn')
     STATICFILES_DIRS = (
-        os.path.join(BASE_DIR, '/static/'),
+        os.path.join(BASE_DIR, 'static'),
     )
 
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
