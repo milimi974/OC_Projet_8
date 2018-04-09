283a971532b150896765037871d8bfdab5c0f9f3
solon <milimi974@gmail.com>
2018-04-09 16:29:23 +0200

    debug heroku

--- master/settings/__init__.py
+++ master/settings/__init__.py
@@ -149,6 +149,10 @@ INTERNAL_IPS = ['127.0.0.1']
 if os.environ.get('ENV') == 'PRODUCTION':
     PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static_cdn')
+    STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, '/static/'),
+    )
+
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
     db_from_env = dj_database_url.config(conn_max_age=500)
     DATABASES['default'].update(db_from_env)
